ff0e91567d0e78d525a339be77d6134fce51fc7d
prajwal3210 <prajwaltalap@gmail.com>
2024-02-29 20:09:54 +0530

    added tailwind configuration and redux store

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,31 @@
+import { useDispatch } from "react-redux";
 import "./App.css";
+import { useState, useEffect } from "react";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
 
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch;
 
-  return (
-    <>
-      <h1>Blog</h1>
-    </>
-  );
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  });
+
+  return !loading ? (
+    <div className=" text-3xl">
+      <h1>Test</h1>
+    </div>
+  ) : null;
 }
 
 export default App;
