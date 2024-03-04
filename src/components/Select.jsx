f87507ca8567966a5f85c304b9cddfc8455243a1
prajwal3210 <prajwaltalap@gmail.com>
2024-03-04 23:09:30 +0530

    ,

---
+++ src/components/Select.jsx
@@ -0,0 +1,19 @@
+import React, { useId } from "react";
+
+function Select({ options, label, className, ...props }, ref) {
+  const id = useId();
+  return (
+    <div className="w-full">
+      {label && <label htmlFor={id} className=""></label>}
+      <select {...props} id={id} ref={ref} className={`${classname}`}>
+        {options?.map((option) => (
+          <option key={option} value={option}>
+            {option}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+}
+
+export default Select;
